82d15d79e2facbad2094c12e97934996747f3738
Nykänen Juuso E V <nykanenj@da5-cs-bk107-22.helsinki.fi>
2017-03-23 18:14:03 +0200

    Rough versions of website content

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,31 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  echo 'Tämä on etusivu!';
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      View::make('helloworld.html');
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+
+    public static function overview() {
+        View::make('suunnitelmat/overview.html');
+    }
+
+    public static function query() {
+        View::make('suunnitelmat/query.html');
+    }
+
+    public static function query_results() {
+        View::make('suunnitelmat/query_results.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,25 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/login', function() {
+    HelloWorldController::login();
+});
+
+$routes->get('/overview', function() {
+    HelloWorldController::overview();
+});
+
+$routes->get('/query', function() {
+    HelloWorldController::query();
+});
+
+$routes->get('/query_results', function() {
+    HelloWorldController::query_results();
+});
