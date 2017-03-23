42e23c56ceca856024a52a947e69840b9f792440
Nykänen Juuso E V <nykanenj@da5-cs-b221-06.pc.helsinki.fi>
2017-03-23 15:37:27 +0200

    Viikko 2 lisäyksiä

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="nykanenj"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="nykanenj.users.cs.helsinki.fi/Tsoha"
+PROJECT_FOLDER="Tsoha"  #nykanenj.users.cs.helsinki.fi/Tsoha"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
