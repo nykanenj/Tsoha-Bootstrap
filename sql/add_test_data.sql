df9aa4d712c756e5dcb9f177ce7ac7ff10b2664c
Nykänen Juuso E V <nykanenj@da5-cs-bk107-12.pc.helsinki.fi>
2017-03-27 14:25:21 +0300

    Testidatan lisäyslausekkeet

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,41 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO usertable (user_name, password) VALUES ('Testikäyttäjä', 'Testisala');
+INSERT INTO usertable (user_name, password) VALUES ('Matti Nykänen', 'Mäkikotka');
+
+INSERT INTO customercompany (company_name, vat_number) VALUES ('Mummonpullat Oy', '1234567-8');
+INSERT INTO customercompany (company_name, vat_number) VALUES ('Sente Academy', '2663047-3');
+
+INSERT INTO question (qID, question) VALUES ('tunnettuus', 'Tunnetko tämän brändin?');
+INSERT INTO question (qID, question) VALUES ('mainonnanmuistaminen', 'Missä olet nähnyt tuotteesta mainontaa?');
+INSERT INTO question (qID, question) VALUES ('maistuminen', 'Miltä pullat maistuu?');
+
+INSERT INTO answer (question_id, answer) VALUES (1, 'Kyllä');
+INSERT INTO answer (question_id, answer) VALUES (1, 'Ei');
+INSERT INTO answer (question_id, answer) VALUES (1, 'Kyllä');
+INSERT INTO answer (question_id, answer) VALUES (1, 'Kyllä');
+INSERT INTO answer (question_id, answer) VALUES (1, 'Ei');
+INSERT INTO answer (question_id, answer) VALUES (2, 'TV');
+INSERT INTO answer (question_id, answer) VALUES (2, 'TV');
+INSERT INTO answer (question_id, answer) VALUES (2, 'Internet');
+INSERT INTO answer (question_id, answer) VALUES (2, 'Internet');
+INSERT INTO answer (question_id, answer) VALUES (2, 'Lehti');
+INSERT INTO answer (question_id, answer) VALUES (2, 'TV');
+INSERT INTO answer (question_id, answer) VALUES (3, 'Hyvältä');
+INSERT INTO answer (question_id, answer) VALUES (3, 'Herkulliselta');
+INSERT INTO answer (question_id, answer) VALUES (3, 'Herkulliselta');
+INSERT INTO answer (question_id, answer) VALUES (3, 'Pahalta');
+
+INSERT INTO questionnaire (company_id, questionnaire_name) VALUES (2, 'Kevään bränditutkimus sente');
+INSERT INTO questionnaire (company_id, questionnaire_name) VALUES (1, 'Mummonpullat pilotti');
+
+INSERT INTO questionnairesquestions (question_id, questionnaire_id) VALUES (3, 2);
+
+
+
+
+
+
+
+
+
+
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -7,18 +7,14 @@ CREATE TABLE usertable(
 CREATE TABLE customercompany(
   company_id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
   company_name varchar(30) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
-  vat_number varchar(8) NOT NULL
+  vat_number char(9) NOT NULL
 );
 
-CREATE TABLE questionnaire(
-  questionnaire_id SERIAL PRIMARY KEY,
-  company_id INTEGER REFERENCES customercompany(company_id), -- Viiteavain CustomerCompany-tauluun
-  questionnaire_name varchar(30)
-);
+
 
 CREATE TABLE question(
   question_id SERIAL PRIMARY KEY,
-  qID varchar(50) --Text seen by users as the question identifier. It identifies similar questions among many questionnaires.
+  qID varchar(50), --Text seen by users as the question identifier. It identifies similar questions among many questionnaires.
   question varchar(50) NOT NULL
 );
 
@@ -28,11 +24,17 @@ CREATE TABLE answer(
   answer varchar(50) NOT NULL
 );
 
+CREATE TABLE questionnaire(
+  questionnaire_id SERIAL PRIMARY KEY,
+  company_id INTEGER REFERENCES customercompany(company_id), -- Viiteavain CustomerCompany-tauluun
+  questionnaire_name varchar(30)
+);
+
+
 CREATE TABLE questionnairesquestions(
   question_id INTEGER REFERENCES question(question_id),
   questionnaire_id INTEGER REFERENCES questionnaire(questionnaire_id)
 );
-
 
 
 
