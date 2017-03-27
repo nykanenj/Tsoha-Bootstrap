c5db020593b402c6faccdda7916c0f920199e066
Nykänen Juuso E V <nykanenj@da5-cs-b221-23.pc.helsinki.fi>
2017-03-27 17:06:49 +0300

    muutoksia tietokantatauluihin ja testidatan lisäämistä

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -4,39 +4,29 @@ CREATE TABLE usertable(
   password varchar(30) NOT NULL
 );
 
-CREATE TABLE customercompany(
-  company_id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
-  company_name varchar(30) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
-  vat_number char(9) NOT NULL
+CREATE TABLE questiondata(
+  questiondata_id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+  project_start DATE, 
+  questionnaire_name varchar(40) NOT NULL,
+  customer_company varchar(40) NOT NULL, 
+  vat_number char(9) NOT NULL,
+  question varchar(50) NOT NULL,
+  qID varchar(30) NOT NULL,
+  answer varchar(40) NOT NULL
 );
 
-
-
-CREATE TABLE question(
-  question_id SERIAL PRIMARY KEY,
-  qID varchar(50), --Text seen by users as the question identifier. It identifies similar questions among many questionnaires.
-  question varchar(50) NOT NULL
+CREATE TABLE respondent(
+  respondent_id SERIAL PRIMARY KEY,
+  respondent_name varchar(40), 
+  gender varchar(10),
+  age INTEGER,
+  city varchar(20),
+  address varchar(50)
+ 
 );
 
-CREATE TABLE answer(
-  answer_id SERIAL PRIMARY KEY,
-  question_id INTEGER REFERENCES question(question_id),
-  answer varchar(50) NOT NULL
+CREATE TABLE questionrespondent(
+  questiondata_id INTEGER REFERENCES questiondata(questiondata_id),
+  respondent_id INTEGER REFERENCES respondent(respondent_id)
 );
-
-CREATE TABLE questionnaire(
-  questionnaire_id SERIAL PRIMARY KEY,
-  company_id INTEGER REFERENCES customercompany(company_id), -- Viiteavain CustomerCompany-tauluun
-  questionnaire_name varchar(30)
-);
-
-
-CREATE TABLE questionnairesquestions(
-  question_id INTEGER REFERENCES question(question_id),
-  questionnaire_id INTEGER REFERENCES questionnaire(questionnaire_id)
-);
-
-
-
-
 
